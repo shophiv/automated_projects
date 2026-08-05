f313f69e38a42b1a50218261318c262a74feef17
mustansar <mustansartanwir11e@gmail.com>
2026-08-05 14:38:49 +0500

    Delete backend directory

--- backend/src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './app';
-import { logger } from './config/logger';
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  logger.info(`Server is running on port ${PORT}`);
-});
