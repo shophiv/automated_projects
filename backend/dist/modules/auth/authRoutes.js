49c11de1997262af6bcd874c1eebfbe8ad379d02
mustansar <mustansartanwir11e@gmail.com>
2026-08-07 19:20:55 +0500

    Delete backend directory

--- backend/dist/modules/auth/authRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authController_1 = require("./authController");
-const router = (0, express_1.Router)();
-const authController = new authController_1.AuthController();
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-exports.default = router;
